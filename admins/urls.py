cfb6322b43d71af6f89f626a910581e2e39344e1
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-09-07 20:43:45 +0530

    admin pages created

--- admins/urls.py
+++ admins/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_home, name='admin_home'),
+    path('admin-profile/', views.admin_profile, name='admin_profile'),
+    path('admin-routes/', views.admin_routes, name='admin_routes'),
+    path('admin-users/', views.admin_users, name='admin_users'),
+    path('admin-transactions/', views.admin_transactions, name='admin_transactions'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -3,8 +3,28 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 
-#Home
+# Admin Home
 def admin_home(request):
     if 'super_username' not in  request.session:
         return redirect('login_user') 
     return render(request, 'admins_temp/admin-home.html')
+
+
+# Admin Profile
+def admin_profile(request):
+    return render(request, 'admins_temp/admin-profile.html')
+
+
+# Admin Routes
+def admin_routes(request):
+    return render(request, 'admins_temp/admin-routes.html')
+
+
+# Admin Users
+def admin_users(request):
+    return render(request, 'admins_temp/admin-users.html')
+
+
+# Admin Transactions
+def admin_transactions(request):
+    return render(request, 'admins_temp/admin-transactions.html')
