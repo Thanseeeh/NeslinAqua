7af9667d04200281bfc7f3441312fc3d5878fb96
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-09-25 06:05:48 +0530

    updates

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user-profile/', views.profile, name='profile'),
+    path('add-store/', views.add_store, name='add_store'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 
-#Home
+# Home
 def home(request):
     if 'username' not in  request.session:
         return redirect('login_user') 
     return render(request, 'users_temp/index.html')
+
+
+# Profile
+def profile(request):
+    return render(request, 'users_temp/profile.html')
+
+
+# AddStore
+def add_store(request):
+    return render(request, 'users_temp/add_store.html')
