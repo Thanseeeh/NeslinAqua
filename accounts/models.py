0ef9230a4436d01bf7347423873e840b4281c3e5
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-10-06 17:19:03 +0530

    store adding

--- accounts/models.py
+++ accounts/models.py
@@ -36,7 +36,7 @@ class Account(AbstractBaseUser):
     username    = models.CharField(max_length=100, unique=True)
 
     date_joined = models.DateTimeField(auto_now_add=True)
-    last_login  = models.DateTimeField(auto_now_add=True)
+    last_login  = models.DateTimeField()
     is_admin    = models.BooleanField(default=False)
     is_staff    = models.BooleanField(default=False)
     is_active   = models.BooleanField(default=False)
--- users/models.py
+++ users/models.py
@@ -10,9 +10,9 @@ class Store(models.Model):
     price_for_jar   = models.FloatField()
     stand           = models.IntegerField(blank=True, null=True, default=0)
     dispencer       = models.IntegerField(blank=True, null=True, default=0)
-    jar             = models.IntegerField(blank=True, null=True, default=0)
     route           = models.ForeignKey(Account, on_delete=models.CASCADE)
     is_active       = models.BooleanField(default=True)
+    created_date    = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
